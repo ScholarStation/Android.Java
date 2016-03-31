29833d24bc0b98769a028c67d7a448f80c1109d7
Ben <bjc9001@gmail.com>
2016-03-31 17:25:52 -0500

    SG

--- ScholarStation/app/src/main/java/scholarstationandroid/scholarstation/StudyGroupActivity.java
+++ ScholarStation/app/src/main/java/scholarstationandroid/scholarstation/StudyGroupActivity.java
@@ -1,29 +1,86 @@
 package scholarstationandroid.scholarstation;
-import android.app.Activity;
+
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
-/**
- * Created by jonat on 3/30/2016.
- */
 
-public class StudyGroupActivity extends Activity {
+import WebUtil.StudySession.StudyGroup;
 
-    TextView personName;
-    TextView personAge;
-    ImageView personPhoto;
+public class StudyGroupActivity extends AppCompatActivity {
+
+    private RecyclerView Rview;
+    private Adapter SGAdapter;
+    private RecyclerView.LayoutManager SGLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_study_group);
-        personName = (TextView)findViewById(R.id.person_name);
-        personAge = (TextView)findViewById(R.id.person_age);
-        personPhoto = (ImageView)findViewById(R.id.person_photo);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        personName.setText("Jimmy Dean");
-        personAge.setText("45 years old");
-        personPhoto.setImageResource(R.drawable.fab_button);
+        Rview = (RecyclerView) findViewById(R.id.StudyGroup_recycler_view);
+        Rview.setHasFixedSize(true);
+
+        SGLayoutManager = new LinearLayoutManager(this);
+        Rview.setLayoutManager(SGLayoutManager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
+ class SGAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+        private StudyGroup[] SGList;
+
+     public static class ViewHolder extends RecyclerView.ViewHolder {
+         // each data item is just a string in this case ????????????????????????????????????
+         public TextView mTextView;
+         public ViewHolder(TextView v) {
+             super(v);
+             mTextView = v;
+         }
+     }
+
+     public SGAdapter(StudyGroup[] SGList) {
+         this.SGList = SGList;
+     }
+
+
+
+     @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+         View v = LayoutInflater.from(parent.getContext())
+                 .inflate(R.layout.content_study_group, parent, false);
+         // set the view's size, margins, paddings and layout parameters
+
+         ViewHolder vh = new ViewHolder((TextView) v);
+         return vh;
+    }
+
+
+     @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
