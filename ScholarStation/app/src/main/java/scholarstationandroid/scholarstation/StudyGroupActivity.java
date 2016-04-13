1493de084cca282ed166cd97c80a1c248d17081c
Ben <bjc9001@gmail.com>
2016-04-13 18:53:43 -0500

    moved SG adapter into Activity

--- ScholarStation/app/src/main/java/scholarstationandroid/scholarstation/StudyGroupActivity.java
+++ ScholarStation/app/src/main/java/scholarstationandroid/scholarstation/StudyGroupActivity.java
@@ -1,12 +1,14 @@
 package scholarstationandroid.scholarstation;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Movie;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,8 +16,10 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.Adapter;
 import android.support.v7.widget.Toolbar;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,6 +29,8 @@ import java.util.List;
 import java.util.ListIterator;
 import WebUtil.Login.LoginReq;
 import WebUtil.Login.LoginRes;
+import WebUtil.StudySession.DeleteStudyReq;
+import WebUtil.StudySession.DeleteStudyRes;
 import WebUtil.StudySession.StudyGroupReq;
 import WebUtil.StudySession.StudyGroupRes;
 import WebUtil.Webutil;
@@ -135,7 +141,127 @@ public class StudyGroupActivity extends AppCompatActivity {
     public void setAddGroupButton(){
 
     }
+    class SGAdapter extends RecyclerView.Adapter<SGAdapter.MyViewHolder> {
+        private List<StudyGroup> groupList;
+        StudyGroup group;
 
+
+        int Posistion;
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView title, subject, date;
+            private View v;
+            public View getView(){
+                return v;
+            }
+
+            public MyViewHolder(View v) {
+                super(v);
+                this.v=v;
+
+                title = (TextView) v.findViewById(R.id.title);
+                subject = (TextView) v.findViewById(R.id.subject);
+                date = (TextView) v.findViewById(R.id.date);
+            }
+        }
+
+        public SGAdapter(List<StudyGroup> groupList) {
+            this.groupList = groupList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, final int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.studygroup_list_row, parent, false);
+
+
+
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, final int position) {
+
+            holder.getView().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View itemView) {
+                    //Intent myIntent = new Intent(StudyGroupActivity.this , ViewStudyGroup.class);
+                    //startActivity(myIntent);
+                }
+            });
+
+            holder.getView().setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(final View itemView) {
+
+                    final CharSequence[] selectors = {"Edit", "Delete"};
+                    AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
+                    builder.setTitle("Choose an action:");
+                    AlertDialog.Builder builder1 = builder.setItems(selectors, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            if (which == 0) {
+                                System.out.println("Got to edit++++++++++++++++++++++++++++++++++");
+
+                            } else {
+                                //System.out.println("This is group ID " + groupList.get(viewType)._id + "=========================");
+                                class NetworkCallTask extends AsyncTask<Object, Object, Object> {
+                                    @Override
+                                    protected void onPreExecute() {
+                                        super.onPreExecute();
+                                    }
+
+                                    @Override
+                                    protected Object doInBackground(Object... params) {
+                                        DeleteStudyReq req = new DeleteStudyReq();
+                                        req._id = groupList.get(position)._id;
+                                        req.KEY = LoginInfo.KEY;
+                                        req.username = LoginInfo.username;
+                                        DeleteStudyRes deleteStudyRes = (DeleteStudyRes) new Webutil().webRequest(req);
+                                        return deleteStudyRes;
+
+                                    }
+
+                                    @Override
+                                    protected void onProgressUpdate(Object... values) {
+                                        super.onProgressUpdate(values);
+                                    }
+
+                                    @Override
+                                    protected void onPostExecute(Object o) {
+                                        try {
+                                            System.out.println("We Deleted IT==========================================");
+
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                                new NetworkCallTask().execute(new Object());
+
+                            }
+
+                        }
+                    });
+
+                    builder.show();
+                    return true;
+                }
+            });
+            StudyGroup group = groupList.get(position);
+
+            holder.title.setText(group.topic);
+            holder.subject.setText(group.course);
+            holder.date.setText(group.date);
+        }
+
+        @Override
+        public int getItemCount() {
+            return groupList.size();
+        }
+
+
+    }
 }
 
 
