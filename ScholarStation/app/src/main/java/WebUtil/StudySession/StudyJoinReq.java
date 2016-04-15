871063d11761e4b29b230f472245921978662732
bmp18 <bmp18@students.uwf.edu>
2016-04-15 14:32:10 -0500

    added joinStudy xml
    this will be used to join study groups

---
+++ ScholarStation/app/src/main/java/WebUtil/StudySession/StudyJoinReq.java
@@ -0,0 +1,11 @@
+package WebUtil.StudySession;
+
+import WebUtil.WebRequest;
+
+/**
+ * Created by Branden on 4/15/2016.
+ */
+public class StudyJoinReq extends WebRequest {
+    public String _id;
+    public String newMember;
+}
---
+++ ScholarStation/app/src/main/java/WebUtil/StudySession/StudyJoinRes.java
@@ -0,0 +1,9 @@
+package WebUtil.StudySession;
+
+import WebUtil.WebResponse;
+
+/**
+ * Created by Branden on 4/15/2016.
+ */
+public class StudyJoinRes extends WebResponse {
+}
